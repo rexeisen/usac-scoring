b3269306ff6f156fbe2e9038041cc9a21c12f567
Jon Rexeisen <jon.rexeisen@gmail.com>
2023-03-17 17:36:15 -0500

    Added ability to get the best attempt from a routecard

--- USA Climbing Scoring/ViewModels/Models/RouteCard/RouteCard.swift
+++ USA Climbing Scoring/ViewModels/Models/RouteCard/RouteCard.swift
@@ -26,12 +26,61 @@ struct RouteCard: Codable, Hashable {
         case routeId = "rid"
     }
     
-    let attempts: [String]
+    let attempts: [Attempt]
     let climberId: String
     let routeId: String
+    
+    var bestAttempt: Attempt? {
+        return attempts.sorted(by: >).first
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let rawAttemptString = try container.decode([String].self, forKey: .attempts)
+        var builtAttempts: [Attempt] = []
+        for (index, element) in rawAttemptString.enumerated() {
+            builtAttempts.append(Attempt(score: element, attempt: index + 1))
+        }
+        
+        self.attempts = builtAttempts
+        self.climberId = try container.decode(String.self, forKey: .climberId)
+        self.routeId = try container.decode(String.self, forKey: .routeId)
+    }
     
     static func == (lhs: RouteCard, rhs: RouteCard) -> Bool {
         return lhs.climberId == rhs.climberId && lhs.routeId == rhs.routeId
     }
     
+}
+
+struct Attempt: Codable, Hashable, Comparable {
+    static func < (lhs: Attempt, rhs: Attempt) -> Bool {
+        return lhs.value < rhs.value
+    }
+    
+    let value: Double
+    let score: String
+    let attempt: Int
+    
+    init(score: String, attempt: Int) {
+        
+        var localScore: Double = 0.0
+        
+        if score.lowercased() == "top" {
+            localScore = 100
+        } else if score.contains("+") {
+            let strippedScore = score.replacingOccurrences(of: "+", with: "")
+            let rawValue = Double(strippedScore) ?? 0.0
+            localScore = rawValue + 0.5
+        } else {
+            localScore = Double(score) ?? 0.0
+        }
+        
+        localScore -= (Double(attempt) * 0.001)
+        
+        self.value = localScore
+        self.score = score
+        self.attempt = attempt
+    }
 }
--- USA Climbing ScoringTests/USA_Climbing_ScoringTests.swift
+++ USA Climbing ScoringTests/USA_Climbing_ScoringTests.swift
@@ -25,12 +25,164 @@ final class USA_Climbing_ScoringTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRouteSortLastBest() throws {
+        let example = """
+{
+"attempts": [
+  "9",
+  "10",
+  "12"
+],
+"climbTime": "02:58.58",
+"disc": "leadtr",
+"eid": "1188",
+"jid": 3373,
+"lid": "L5",
+"live": 1,
+"mid": "29670274",
+"rid": "R10",
+"round": "final",
+"tsRecv": 1677952378677,
+"tsSent": 1677952378295,
+"ver": 8
+}
+"""
+        let decoder = JSONDecoder()
+        let data = try XCTUnwrap(example.data(using: .utf8))
+        let routeCard = try decoder.decode(RouteCard.self, from: data)
+        
+        let bestAttempt = try XCTUnwrap(routeCard.bestAttempt)
+        XCTAssertEqual(bestAttempt.score, "12")
+        XCTAssertEqual(bestAttempt.attempt, 3)
+        
+    }
+    
+    func testRouteSortFirstBest() throws {
+        let example = """
+{
+"attempts": [
+  "12",
+  "11",
+  "9"
+],
+"climbTime": "02:58.58",
+"disc": "leadtr",
+"eid": "1188",
+"jid": 3373,
+"lid": "L5",
+"live": 1,
+"mid": "29670274",
+"rid": "R10",
+"round": "final",
+"tsRecv": 1677952378677,
+"tsSent": 1677952378295,
+"ver": 8
+}
+"""
+        let decoder = JSONDecoder()
+        let data = try XCTUnwrap(example.data(using: .utf8))
+        let routeCard = try decoder.decode(RouteCard.self, from: data)
+        
+        let bestAttempt = try XCTUnwrap(routeCard.bestAttempt)
+        XCTAssertEqual(bestAttempt.score, "12")
+        XCTAssertEqual(bestAttempt.attempt, 1)
+        
+    }
+    
+    func testRouteSortTop() throws {
+        let example = """
+{
+"attempts": [
+  "12",
+  "TOP"
+],
+"climbTime": "02:58.58",
+"disc": "leadtr",
+"eid": "1188",
+"jid": 3373,
+"lid": "L5",
+"live": 1,
+"mid": "29670274",
+"rid": "R10",
+"round": "final",
+"tsRecv": 1677952378677,
+"tsSent": 1677952378295,
+"ver": 8
+}
+"""
+        let decoder = JSONDecoder()
+        let data = try XCTUnwrap(example.data(using: .utf8))
+        let routeCard = try decoder.decode(RouteCard.self, from: data)
+        
+        let bestAttempt = try XCTUnwrap(routeCard.bestAttempt)
+        XCTAssertEqual(bestAttempt.score, "TOP")
+        XCTAssertEqual(bestAttempt.attempt, 2)
+        
+    }
+    
+    func testRouteSameScore() throws {
+        let example = """
+{
+"attempts": [
+  "7",
+  "0",
+  "7",
+],
+"climbTime": "02:58.58",
+"disc": "leadtr",
+"eid": "1188",
+"jid": 3373,
+"lid": "L5",
+"live": 1,
+"mid": "29670274",
+"rid": "R10",
+"round": "final",
+"tsRecv": 1677952378677,
+"tsSent": 1677952378295,
+"ver": 8
+}
+"""
+        let decoder = JSONDecoder()
+        let data = try XCTUnwrap(example.data(using: .utf8))
+        let routeCard = try decoder.decode(RouteCard.self, from: data)
+        
+        let bestAttempt = try XCTUnwrap(routeCard.bestAttempt)
+        XCTAssertEqual(bestAttempt.score, "7")
+        XCTAssertEqual(bestAttempt.attempt, 1)
+        
+    }
+    
+    func testRouteSameScoreWithPlus() throws {
+        let example = """
+{
+"attempts": [
+  "7",
+  "0",
+  "7+",
+],
+"climbTime": "02:58.58",
+"disc": "leadtr",
+"eid": "1188",
+"jid": 3373,
+"lid": "L5",
+"live": 1,
+"mid": "29670274",
+"rid": "R10",
+"round": "final",
+"tsRecv": 1677952378677,
+"tsSent": 1677952378295,
+"ver": 8
+}
+"""
+        let decoder = JSONDecoder()
+        let data = try XCTUnwrap(example.data(using: .utf8))
+        let routeCard = try decoder.decode(RouteCard.self, from: data)
+        
+        let bestAttempt = try XCTUnwrap(routeCard.bestAttempt)
+        XCTAssertEqual(bestAttempt.score, "7+")
+        XCTAssertEqual(bestAttempt.attempt, 3)
+        
     }
 
 }
